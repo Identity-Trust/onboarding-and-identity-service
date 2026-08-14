abfb6c63c512c42bb84ad641751080bb3e967cb1
dajinkya1099 <dajinkya1099@gmail.com>
2026-08-14 17:18:09 +0530

    Initial commit: onboarding_and_identity_service  service

---
+++ src/main/java/com/identityos/onboarding_and_identity_service/OnboardingAndIdentityServiceApplication.java
@@ -0,0 +1,18 @@
+package com.identityos.onboarding_and_identity_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+
+@SpringBootApplication(exclude = {
+        DataSourceAutoConfiguration.class,
+        HibernateJpaAutoConfiguration.class
+})
+public class OnboardingAndIdentityServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnboardingAndIdentityServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/identityos/onboarding_and_identity_service/OnboardingAndIdentityServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.identityos.onboarding_and_identity_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnboardingAndIdentityServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
